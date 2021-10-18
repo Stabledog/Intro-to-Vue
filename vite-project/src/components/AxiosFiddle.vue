e1aec513dd6e6bfe2a2d790ba7990525e2ab4663
Les Matheson <lmatheson4@bloomberg.net>
2021-10-17 20:24:42 -0400

    vite-project with AxiosFiddle almost works, missing import of axios from cdn

---
+++ vite-project/src/components/AxiosFiddle.vue
@@ -0,0 +1,34 @@
+<script fefoo>
+// AxiosFiddle.js
+import { ref } from 'vue'
+
+//const axios = require('axios');
+export default {
+    props: {
+        baseUrl: {
+            type: String,
+            required: false
+        }
+    },
+    data() {
+        return {
+            todos: undefined
+        };
+    },
+    methods: {
+        test1() {
+            axios.get('https://jsonplaceholder.typicode.com/todos/1')
+                .then( r => { this.todos=[r.data];} )
+                .catch ( e => { console.error(e);} );
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div> <h3>Axios Fiddle</h3>
+        <div>{{todos}}</div>
+        <button type=button @click="test1">Fetch</button>
+    </div>
+</template>
